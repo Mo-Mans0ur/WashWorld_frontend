8870e186ef918265646b7dcbdf1e0fcb5a800d0c
Mo-Mansour <Moha1998@outlook.dk>
2026-05-11 15:33:44 +0200

    removed the <br /> tags and added margin-bottom to the elements to create spacing instead.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -13,17 +13,17 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="absolute inset-0 w-full h-full overflow-hidden z-10 ">
+      <div className="absolute inset-0 w-full h-full overflow-hidden z-10  ">
         <Image
           src="/background/washworld-background.png"
           alt="Baggrund"
           fill
           priority
         />
-        <br />
+       
         <div className="absolute inset-0 bg-(--color-overlay-dark-40) z-20" />
-        <div className="relative z-20 flex flex-col items-center justify-center h-full gap-4">
-          <h2 className="-mt-4 text-white text-3xl font-bold">Velkommen til</h2>
+        <div className="relative z-20 flex flex-col items-center justify-center h-full gap-4 ">
+          <h2 className="-mt-4 text-white text-3xl font-bold ">Velkommen til</h2>
 
           <Image
             src="/logos/washworld-white.png"
@@ -31,9 +31,10 @@ export default function LoginPage() {
             width={234}
             height={102}
             priority
+            className="mb-18 "
           />
 
-          <br />
+         
 
           <input
             placeholder="Brugernavn"
@@ -42,9 +43,9 @@ export default function LoginPage() {
           <input
             placeholder="Kodeord"
             type="password"
-            className="w-72 p-3 bg-(--color-surface)"
+            className="w-72 p-3 bg-(--color-surface) mb-7"
           />
-          <br />
+          
           <LoginButton onLoginClick={handleLoginClick} />
         </div>
       </div>
