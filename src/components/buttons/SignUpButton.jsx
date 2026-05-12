237ea369665e744a9408579542afe6b768c39882
alextextase <alextextase@gmail.com>
2026-05-12 10:05:47 +0200

    signup-page

--- src/components/buttons/SignUpButton.jsx
+++ src/components/buttons/SignUpButton.jsx
@@ -1,21 +1,23 @@
-export default function SignUpButton() {
+export default function SignUpButton({ onLoginClick, onSignupClick }) {
   return (
-    <div className="flex w-full max-w-102.5">
+    <div className="flex w-full max-w-60.5">
       <button
         type="button"
-        className="z-10 flex h-7.5 flex-[1.5] items-center justify-center bg-(--brand-green-01) [clip-path:polygon(0_0,100%_0,89%_100%,0_100%)]"
+        onClick={onLoginClick}
+        className=" z-10 flex h-7.5 flex-[1.02] items-center justify-center bg-(--brand-green-01) [clip-path:polygon(0_0,100%_0,83%_100%,0_100%)]"
       >
-        <span className="text-[0.72rem] font-bold tracking-tight text-white">
-          Opret konto
+        <span className="text-xl font-bold tracking-tight text-white">
+          Log ind
         </span>
       </button>
 
       <button
         type="button"
-        className="-ml-5 flex h-7.5 flex-[0.7] items-center justify-center bg-(--color-grey-01) px-3 [clip-path:polygon(19%_0,100%_0,100%_100%,0_100%)]"
+        onClick={onSignupClick}
+        className="-ml-5 flex h-12.5 flex-1 items-center justify-center bg-(--color-grey-01) px-3 [clip-path:polygon(15%_0,100%_0,100%_100%,0_100%)]"
       >
-        <span className="whitespace-nowrap text-[0.62rem] font-medium tracking-tight text-white">
-          Afbryd
+        <span className="whitespace-nowrap text-[0.99rem] font-medium tracking-tight text-white">
+          Ny bruger
         </span>
       </button>
     </div>
