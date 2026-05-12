28113711c56fdcca913f8b6f3b12d30ed5636755
alextextase <alextextase@gmail.com>
2026-05-12 12:17:04 +0200

    page details

---
+++ src/app/details/page.js
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from 'react';
+import Image from "next/image";
+
+
+
+
+<div className="absolute inset-0 w-full h-full overflow-hidden z-10 ">
+    <Image src="/background/washworld-background.png" 
+    alt="Baggrund"
+    fill
+    priority
+    />
+
+
